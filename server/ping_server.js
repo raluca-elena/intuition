483eb1f69f81a36ab59bb83fa4fb4f1e4f664dfe
raluca-elena <ralucaelena1985@gmail.com>
2015-08-12 21:25:27 -0700

    fexed header cooking

--- server/ping_server.js
+++ server/ping_server.js
@@ -39,8 +39,9 @@ app.get("/", function(req, res) {
   console.log("pid of child", child.pid);
   child.stdout.on("data", function (data) {
       console.log("DATA ", data.toString());
+      res.write(data);
   });
-  res.json(x);
+  //res.json(x);
 })
 
 app.put("/", function(req, res, next) {
