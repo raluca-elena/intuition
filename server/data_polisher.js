a9ee0145d4f37e8d15fe017863b0d31bafef1ac1
raluca-elena <ralucaelena1985@gmail.com>
2015-08-13 16:55:39 -0700

    getiing data from g json

--- server/data_polisher.js
+++ server/data_polisher.js
@@ -1,5 +1,16 @@
+var fs = require('fs');
 var json = process.argv[2];
+
 function getRes() {
-  console.log(json.toString());
+  //console.log(json);
+  var x = fs.readFileSync(json, 'utf8');
+  var data = JSON.parse(x);
+  console.log("length of data" , data.results.length);
+  for (i = 0; i < data.results.length; i++) {
+    console.log("data is ", data.results[i].name);
+    console.log("place id ", data.results[i].place_id);
+    console.log("type", data.results[i].types);
+  }
 }
+
 getRes();
